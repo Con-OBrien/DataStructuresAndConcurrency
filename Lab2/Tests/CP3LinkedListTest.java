8e0138970a154139edb2dfd6c086070971483db4
Conor O Brien <t00193238@365s.ittralee.ie>
2018-09-24 12:55:52 +0100

    Still have to work on this

--- Lab2/Tests/CP3LinkedListTest.java
+++ Lab2/Tests/CP3LinkedListTest.java
@@ -1,22 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CP3LinkedListTest {
 
+    CP3LinkedList e;
     @Test
     void getFirst() {
-
+        e.addFirst("Bob");
+        assertEquals("Bob", e.getFirst());
     }
-
+    @BeforeEach
+    void start() {
+        e = new CP3LinkedList();
+    }
     @Test
     void addFirst() {
-
+        e.addFirst("Bob");
+        assertEquals("Bob",e.getFirst());
     }
 
     @Test
     void removeFirst() {
-
+        e.addFirst("John");
+        assertEquals("John",e.removeFirst());
     }
 
     @Test
@@ -26,5 +34,11 @@ class CP3LinkedListTest {
 
         list.addFirst("Brad");
         assertFalse(list.isEmpty());
+    }
+
+
+    @Test
+    void isSizeEmpty() {
+
     }
 }
--- Lab2/src/CP3LinkedList.java
+++ Lab2/src/CP3LinkedList.java
@@ -1,36 +1,93 @@
 import java.util.NoSuchElementException;
+//singly linked list with first reference only 
+
+/**
+A linked list is a sequence of links with efficient
+element insertion and removal. This class 
+contains a subset of the methods of the standard
+java.util.LinkedList class.
+*/
 
 public class CP3LinkedList<E> {
-    private class Node {
-        public E data;
-        public Node next;
-    }
-    private Node first;
+     private class Node {
+		public E data;
+		public Node next;
+	}
+	private Node first;
+		
+	/** 
+    	Constructs an empty linked list.
+	*/
+	public CP3LinkedList()
+	{  
+		first = null;
+	}
+ 
+	/**
+ 	Adds an element to the front of the linked list.
+ 	@param element the element to add
+	 */
+	public void addFirst(E element)
+	{  
+		Node newLink = new Node();
+		newLink.data = element;
+		newLink.next = first;
+		first = newLink;
+	}
 
-    public CP3LinkedList() {
-        first = null;
+	public E getFirst() {
+		if (first == null)
+			throw new NoSuchElementException();
+		return first.data;
+	}
+
+	public E removeFirst() {
+		if (first == null)
+			throw new NoSuchElementException();
+		E element = first.data;
+		first = first.next;
+		return element;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+	public void print() {
+		Node current = first;
+		while (current != null) {
+	         System.out.println(current.data);
+	         current = current.next;
+	      }
+	}
+
+	public int size() {
+	    int size = 0;
+	    Node current = first;
+
+	    while (current != null) {
+
+	        size++;
+	        current = current.next;
+        }
+	    return size;
     }
-    public E getFirst() {
-        if (first == null)
-            throw new NoSuchElementException();
-        return first.data;
+    public void addLast(E last) {
+	    Node current = first;
+	    while(current != null) {
+	        current = current.next;
+        }
+        current.data = last;
     }
 
-    public void addFirst(E element) {
-        Node node = new Node();
-        node.data = element;
-        node.next = null;
-            first = node;
-    }
-
-    public E removeFirst() {
-        if (first == null)
-            throw new NoSuchElementException();
-        E element = first.data;
-        first = first.next;
-        return element;
-    }
-    public boolean isEmpty() {
-        return first == null;
+    public E getLast() {
+	    Node current = first;
+	    String last;
+	    if(current == null)
+	        throw new NoSuchElementException();
+	    while(current != null)
+        {
+            current = current.next;
+        }
+        return null;
     }
 }
